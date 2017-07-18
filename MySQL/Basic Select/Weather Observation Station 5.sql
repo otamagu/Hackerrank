e6f59b7ba53954bcc07228a088103e665d4ab9e2
enjeru <ten79ryuu@hotmail.com>
2017-07-18 16:17:21 +0800

    Create Weather Observation Station 5.sql

---
+++ MySQL/Basic Select/Weather Observation Station 5.sql
@@ -0,0 +1,13 @@
+//Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: 
+//number of characters in the name). If there is more than one smallest or largest city, choose the one that comes 
+//first when ordered alphabetically.
+
+SELECT city, LENGTH(city)
+FROM STATION
+ORDER BY LENGTH (CITY) ASC, CITY
+LIMIT 1;
+
+SELECT city, LENGTH(city)
+FROM STATION
+ORDER BY LENGTH (CITY) DESC, CITY
+LIMIT 1;
