a8234a2a48fe7e260c2eabdd2eb861c3839b2878
enjeru <ten79ryuu@hotmail.com>
2017-07-18 18:47:58 +0800

    Create Weather Observation Station 8.sql

---
+++ MySQL/Basic Select/Weather Observation Station 8.sql
@@ -0,0 +1,8 @@
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both 
+their first and last characters. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE left(city,1) REGEXP'[aiueo]' and right(city,1) REGEXP'[aiueo]'
