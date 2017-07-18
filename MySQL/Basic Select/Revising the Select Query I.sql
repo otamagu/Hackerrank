5a52ea360b7b5b1327ccfc3eb039f9ab3d463e33
enjeru <ten79ryuu@hotmail.com>
2017-07-18 16:18:04 +0800

    Update Revising the Select Query I.sql

--- MySQL/Basic Select/Revising the Select Query I.sql
+++ MySQL/Basic Select/Revising the Select Query I.sql
@@ -1,3 +1,6 @@
+//Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for 
+//America is USA.
+
 SELECT *
 FROM CITY
 WHERE Population > 100000 and CountryCode LIKE 'USA';
