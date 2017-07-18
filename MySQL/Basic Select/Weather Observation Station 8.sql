d691a5ac2d63a2aa629b6334eb5673b9234b0451
enjeru <ten79ryuu@hotmail.com>
2017-07-18 18:48:33 +0800

    Update Weather Observation Station 8.sql

--- MySQL/Basic Select/Weather Observation Station 8.sql
+++ MySQL/Basic Select/Weather Observation Station 8.sql
@@ -6,3 +6,8 @@ their first and last characters. Your result cannot contain duplicates.
 SELECT DISTINCT CITY
 FROM STATION
 WHERE left(city,1) REGEXP'[aiueo]' and right(city,1) REGEXP'[aiueo]'
+
+/*OR*/
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[AIUEO].*[AIUEO]$';
