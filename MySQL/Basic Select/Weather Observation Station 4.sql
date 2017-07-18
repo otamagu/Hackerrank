3bdb74c8ab844dcadcace680ec4bda4a58a05d98
enjeru <ten79ryuu@hotmail.com>
2017-07-18 18:35:30 +0800

    Update Weather Observation Station 4.sql

--- MySQL/Basic Select/Weather Observation Station 4.sql
+++ MySQL/Basic Select/Weather Observation Station 4.sql
@@ -1,2 +1,9 @@
+/*
+Let N be the number of CITY entries in STATION, and let N' be the number of distinct CITY
+names in STATION; query the value of N - N' from STATION. In other words, find the 
+difference between the total number of CITY entries in the table and the number of distinct
+CITY entries in the table.
+*/
+
 SELECT Count(City)-Count(distinct city)
 FROM STATION;
