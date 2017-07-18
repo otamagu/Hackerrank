e44b4a9ea711c2e1d81f6e415e2dee61b571797e
enjeru <ten79ryuu@hotmail.com>
2017-07-18 23:45:47 +0800

    Create Weather Observation Station 18.sql

---
+++ MySQL/Aggregations/Weather Observation Station 18.sql
@@ -0,0 +1,13 @@
+/*
+Consider p1(a,b) and p2(c,d) to be two points on a 2D plane.
+
+1. a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+2. b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+3. c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+4. d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.
+*/
+
+select round(max(lat_n)- min(lat_n) + max(long_w)-min(long_w),4)
+FROM STATION
